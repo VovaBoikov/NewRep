5b4d11489fff8cf47c655aa6167f291bc3e52a6c
Vova <vladimyrvaleriyovich@gmail.com>
2021-12-11 20:24:49 +0200

    Add bash script

---
+++ Vova12.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+mkdir -p C:/Users/User/Desktop/Vova/@mail
+#
+touch C:/Users/User/Desktop/Vova/@mail/@mail.txt
+#
+echo vova.boikov@mail.ru | tee C:/Users/User/Desktop/Vova/@mail/@mail.txt
+#
+mkdir -p C:/Users/User/Desktop/Vova/Surname
+#
+touch C:/Users/User/Desktop/Vova/Surname/Surname.txt
+#
+echo Boikov | tee C:/Users/User/Desktop/Vova/Surname/Surname.txt
+#
+mkdir -p C:/Users/User/Desktop/Vova/Name
+#
+touch C:/Users/User/Desktop/Vova/Name/Name.txt
+#
+echo Vova | tee C:/Users/User/Desktop/Vova/Name/Name.txt
+#
+mkdir -p C:/Users/User/Desktop/Vova/DateAndGroupNumber
+#
+touch C:/Users/User/Desktop/Vova/DateAndGroupNumber/Group.txt
+#
+echo 151 | tee C:/Users/User/Desktop/Vova/DateAndGroupNumber/Group.txt
+#
+touch C:/Users/User/Desktop/Vova/DateAndGroupNumber/Date.txt
+#
+echo 03.12.21 | tee C:/Users/User/Desktop/Vova/DateAndGroupNumber/Date.txt
+#
+#END
+
+
+
+
